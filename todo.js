eecef086870de378a94099d90d311f8ddf455f30
Chauvin <chauvin-e@orange.fr>
2018-04-30 19:03:52 +0200

    Creation of addTodo()

--- todo.js
+++ todo.js
@@ -1,9 +1,16 @@
 angular.module('todolistApp', [])
 	// Create a controller
 	.controller('TodoListController', function() {
-		var todolist = this;
-		todolist.todos = [
+		var todoList = this;
+		// Create a initial list of todos.
+		todoList.todos = [
 			{text:'Learn AngularJS', done:true},
 			{text: 'Build an AngularJS app', done:false}];
 
+		// Add todos to the list. 
+		todoList.addTodo = function() {
+			// Array.push method used with AngularJS.
+			todoList.todos.push({text:todoList.todoText, done:false});
+			todoList.todoText = '';
+		};	
 	});
