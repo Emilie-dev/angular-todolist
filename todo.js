e5b26c3f42632236259b07df250a02c3f517e431
Chauvin <chauvin-e@orange.fr>
2018-04-30 19:26:29 +0200

    Create archive()

--- todo.js
+++ todo.js
@@ -13,4 +13,14 @@ angular.module('todolistApp', [])
 			todoList.todos.push({text:todoList.todoText, done:false});
 			todoList.todoText = '';
 		};	
+
+		// Archive old todos.
+		todoList.archive =function() {
+			var oldTodos = todoList.todos;
+			todoList.todos = [];
+			angular.forEach(oldTodos, function(todo) {
+				// If todos are not done then they are Array.push again to display them.
+				if(!todo.done) todoList.todos.push(todo);
+			});
+		};
 	});
